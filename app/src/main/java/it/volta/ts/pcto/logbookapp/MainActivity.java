b461d9a735b80f4711423878376d98e7f272770f
sebatt90 <sebattack90@gmail.com>
2023-05-22 15:46:27 +0200

    Fetching JSON from URL (WIP)

--- app/src/main/java/it/volta/ts/pcto/logbookapp/MainActivity.java
+++ app/src/main/java/it/volta/ts/pcto/logbookapp/MainActivity.java
@@ -2,12 +2,24 @@ package it.volta.ts.pcto.logbookapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import it.volta.ts.pcto.logbookapp.utils.JSONTask;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // TESTING
+        // TODO: replace this to test other feature
+        JSONTask myJSONTask = new JSONTask();
+        myJSONTask.loadJSON(MainActivity.this,"https://users3.elettra.eu/lpa_app/lpa_jsonexample");
+
+
     }
 }
---
+++ app/src/main/java/it/volta/ts/pcto/logbookapp/utils/JSONTask.java
@@ -0,0 +1,63 @@
+package it.volta.ts.pcto.logbookapp.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import it.volta.ts.pcto.logbookapp.MainActivity;
+
+public class JSONTask
+{
+    private JSONObject root;
+
+    public void loadJSON(Context ctx, String url) {
+
+        // getting a new volley request queue for making new requests
+        RequestQueue volleyQueue = Volley.newRequestQueue(ctx);
+
+        // since the response we get from the api is in JSON, we
+        // need to use `JsonObjectRequest` for parsing the
+        // request response
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                // we are using GET HTTP request method
+                Request.Method.GET,
+                // url we want to send the HTTP request to
+                url,
+                // this parameter is used to send a JSON object to the
+                // server, since this is not required in our case,
+                // we are keeping it `null`
+                null,
+
+                // lambda function for handling the case
+                // when the HTTP request succeeds
+                (Response.Listener<JSONObject>) response -> {
+                    root = response;
+                },
+
+                // lambda function for handling the case
+                // when the HTTP request fails
+                (Response.ErrorListener) error -> {
+                    // make a Toast telling the user
+                    // that something went wrong
+                    
+                    Toast.makeText(ctx, "Some error occurred! Cannot fetch", Toast.LENGTH_LONG).show();
+                    // log the error message in the error stream
+                    Log.e("MainActivity", "error: ${error.localizedMessage}");
+                }
+        );
+
+        // add the json request object created above
+        // to the Volley request queue
+        volleyQueue.add(jsonObjectRequest);
+    }
+}
