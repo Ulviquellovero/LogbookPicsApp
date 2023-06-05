842ffc848990fbe72c142eb3b17ee420232d644b
sebatt90 <sebattack90@gmail.com>
2023-06-05 11:44:09 +0200

    Temp commit

--- app/src/main/java/eu/elettra/logbookpics/activities/AboutActivity.java
+++ app/src/main/java/eu/elettra/logbookpics/activities/AboutActivity.java
@@ -3,7 +3,12 @@ package eu.elettra.logbookpics.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import eu.elettra.logbookpics.R;
 
@@ -13,5 +18,23 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ((ImageButton)findViewById(R.id.git)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open browser with github repo
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(R.string.git_url)));
+                startActivity(browserIntent);
+            }
+        });
+
+        ((ImageButton)findViewById(R.id.docs)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open browser with github repo
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(R.string.readthedocs_url)));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
