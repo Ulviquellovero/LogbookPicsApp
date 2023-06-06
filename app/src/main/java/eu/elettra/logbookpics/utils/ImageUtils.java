df7cc8c8539f01d972bfe14340cd4c842c3cebb8
sebatt90 <sebattack90@gmail.com>
2023-06-06 13:22:11 +0200

    Saving image to Gallery working

--- app/src/main/java/eu/elettra/logbookpics/utils/ImageUtils.java
+++ app/src/main/java/eu/elettra/logbookpics/utils/ImageUtils.java
@@ -3,6 +3,7 @@ package eu.elettra.logbookpics.utils;
 import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -34,28 +35,10 @@ public class ImageUtils {
         return Bitmap.createScaledBitmap(image, width, height, true);
     }
 
-    public static void saveImageJPEG(Bitmap finalBitmap, String image_name) {
-
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root);
-        myDir.mkdirs();
-        String fname = image_name+".jpg";
-        File file = new File(myDir, fname);
-        if (file.exists()) file.delete();
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void saveBitmapToGallery(Context ctx) {
         Bitmap bitmap = QrCodeInfo.imageBitmap;
 
-        String filename = "todo.jpg"; // Name of the file to be saved
+        String filename = Long.toString(System.currentTimeMillis())+".jpg"; // Name of the file to be saved
         File directory = ctx.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
         File file = new File(directory, filename);
@@ -65,6 +48,8 @@ public class ImageUtils {
             outputStream = new FileOutputStream(file);
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
             outputStream.flush();
+
+            scanFile(file, ctx);
         } catch (IOException e) {
             e.printStackTrace();
             // Handle the exception
@@ -78,4 +63,19 @@ public class ImageUtils {
             }
         }
     }
+
+    private static void scanFile(File file, Context ctx) {
+        MediaScannerConnection.scanFile(
+                ctx,
+                new String[]{file.getAbsolutePath()},
+                null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        // Media scan completed, file is now accessible in the Gallery
+                    }
+                }
+        );
+    }
+
 }
